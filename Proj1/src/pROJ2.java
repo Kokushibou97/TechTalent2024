3c979c049dc21afc320efba48647c0deafe66420
Kokushibou97 <136746018+Kokushibou97@users.noreply.github.com>
2024-02-02 11:28:28 +0100

    Update

---
+++ Proj1/src/pROJ2.java
@@ -0,0 +1,13 @@
+
+public class pROJ2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int one = 20;
+		int two = 55;
+		
+		int Sum = one + two;
+System.out.println(one+"+"+two+"="+Sum);
+	}
+
+}
