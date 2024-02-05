8ef45108edae5598e77ea65d29345d613f2eacbe
Kokushibou97 <136746018+Kokushibou97@users.noreply.github.com>
2024-02-05 13:27:43 +0100

    update

--- Feb05/src/Preg4.java
+++ Feb05/src/Preg4.java
@@ -4,9 +4,9 @@ public class Preg4 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int N = 500;
-		System.out.println("El valor incrementado de la variable N es " + (N + 77));
-		System.out.println("El valor decrementado de la variable N es " + (N - 3));
-		System.out.println("El valor duplicado del valor N es " + (N * 2));
+		System.out.println("El valor incrementado de la variable N es " + (N+77));
+		System.out.println("El valor decrementado de la variable N es " + (N-3));
+		System.out.println("El valor duplicado del valor N es " + (N*2));
 	}
 
 }
