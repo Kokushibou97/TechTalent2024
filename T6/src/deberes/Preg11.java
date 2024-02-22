6c28f0da9f07baf1800b559d0439d8f19f5a9106
Kokushibou97 <136746018+Kokushibou97@users.noreply.github.com>
2024-02-22 10:25:53 +0100

    Update

---
+++ T6/src/deberes/Preg11.java
@@ -0,0 +1,10 @@
+package deberes;
+
+public class Preg11 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ T6/src/deberes/Preg12.java
@@ -0,0 +1,10 @@
+package deberes;
+
+public class Preg12 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
