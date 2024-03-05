3a33f309ee7628563c9e7934dcdc1efb6b5bbb4c
Kokushibou97 <136746018+Kokushibou97@users.noreply.github.com>
2024-03-05 09:11:23 +0100

    Update

--- POO/src/U8/Empleado.java
+++ POO/src/U8/Empleado.java
@@ -1,5 +1,19 @@
 package U8;
 
 public class Empleado {
-
+//atributos//
+	private String nombre;
+	private String apellido;
+	private int edad;
+	private double salario;
+	
+	//metodos//
+	public boolean plus (double sueldoPlus) {
+		boolean aumento=false;
+		if (edad>40) {
+			salario+=sueldoPlus;
+			aumento= true;
+		}
+		return aumento;
+	}
 }
