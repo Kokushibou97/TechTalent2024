e059a3c79b1da02c29e0863bb02bd309ac0fbafe
Kokushibou97 <136746018+Kokushibou97@users.noreply.github.com>
2024-03-13 10:35:19 +0100

    Update

--- U7Grupo/src/ejercicio2UN7.java
+++ U7Grupo/src/ejercicio2UN7.java
@@ -1,38 +1,39 @@
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Scanner;
-
+//se importa la iterfaz para recorrer el hashmap//
 import java.util.Iterator;
 
 
 public class ejercicio2UN7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Scanner scanner = new Scanner(System.in);
-
+		Scanner sc = new Scanner(System.in);
+//Aquí se pide al usuario que registre datos//
 		System.out.print("Ingrese la cantidad de productos: ");
-		int cantidadProductos = scanner.nextInt();
+		int cantidadProductos = sc.nextInt();
 		
-		
+//Este Hashmap sirve para guardar los nombres de los productos como claves//
+		//Luego los precios como valores//
 Map<String, Double> PrecioProducto = new HashMap<>();
 		
 		
-		
+		//Después, se crea un bucle para leer los datos de los productos//
 		for (int i = 1; i <= cantidadProductos; i++) {
 			System.out.print("Ingrese el nombre del producto " + i + ": ");
-			String nombreProducto = scanner.next();
+			String nombreProducto = sc.next();
 
 			
 		System.out.print("Ingrese precio producto: ");
-		double precioProductoBruto = scanner.nextDouble();
+		double precioProductoBruto = sc.nextDouble();
 		
 		System.out.print("Es producto de primera necesidad: ");
-		String necesidadIVA =  scanner.next();
+		String necesidadIVA =  sc.next();
 		
 		necesidadIVA = necesidadIVA.toLowerCase();
+
 		double precioProductoNeto;
-		
+//Luego se crea un bucle para asignar un tipo de IVA, según respuesta si/no//
 		if ( necesidadIVA.equals("si")) {
 			precioProductoNeto = (precioProductoBruto*0.04)+precioProductoBruto;
             System.out.println("el IVA sera del 4%: "  + precioProductoBruto*0.04 +
@@ -47,7 +48,7 @@ Map<String, Double> PrecioProducto = new HashMap<>();
 		}
             
 		
-		
+		//Después de todo lo anterior se calcula el precio total//
 		double sumaCompra = 0;
         	for (double m : PrecioProducto.values()) {
 				sumaCompra += m;
@@ -55,12 +56,13 @@ Map<String, Double> PrecioProducto = new HashMap<>();
 
 			double totalCompra = sumaCompra;
 
-				
+				//mostreo del total//
 				System.out.println( "El precio total de la compra sera de: "+ totalCompra);
-				
+				//Aquí se pide el pago al cliente//
 				System.out.print("El pago del cliente sera ");
-				double pagoCliente =  scanner.nextDouble();			
+				double pagoCliente =  sc.nextDouble();			
 //				
+//Aquí se iforma de la diferencia entre la factura y el lo que paga el cliente (devolución)//
 				double cambioCliente = pagoCliente - totalCompra;
 				System.out.println("El cliente pago " + pagoCliente + " y el cambio sera de " + cambioCliente);
 			
