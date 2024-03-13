f866659a758d7cdc4e86788ab953e9922f5d0f6a
Kokushibou97 <136746018+Kokushibou97@users.noreply.github.com>
2024-03-13 09:51:44 +0100

    update

--- U7Grupo/src/one.java
+++ U7Grupo/src/one.java
@@ -1,43 +1,43 @@
-package Deberes;
+
 import java.util.Scanner;
 import java.util.HashMap;
 
 public class one {
 
     public static void main(String[] args) {
-
+//Declaración de variables, mediante hashtable//
         Scanner sc = new Scanner(System.in);
         HashMap<String, Double> notAlum = new HashMap<>();
 
-        //creamos un Array-string con nombres elegidos y asignador por nosotros//
+        //creamos un Array-string con nombres elegidos//
         String[] names = {"Abde", "Alejandro", "Isabelle", "Jose", "Aurora",
                            "Jessica", "Joel", "Manel", "Laia", "Sebas",
                            "Santos", "Diego", "Toni", "Alex", "Anna"};
 
         System.out.println(" Bonjour! somos su calculadora de notas de toda la vida ");
 
-        //Creamos un bucle para asignar notas a los nombres//
+        //Creamos un bucle para pedir notas//
         for (String name : names) {
             System.out.println("Ingrese la nota para " + name);
             double notaMedia = Double.parseDouble(sc.nextLine());
             notAlum.put(name, notaMedia);
         }
-//inecializamos el bucle. cuyo valor es igual a 0 para guardar la información//
+     // Cálculo de la media//
         double sumaNotas = 0;
-        //aquí hacemos el calculo//
         for (double nota : notAlum.values()) {
             sumaNotas += nota;
         }
-        //aqui calculamos la nota media de la suma de todas las notas//
         double notaMediaGlobal = sumaNotas / notAlum.size();
 
-        System.out.println("\nEstadísticas generales");
-        System.out.println("Cantidad de alumnos " + notAlum.size());
-        System.out.println("Nota media general " + notaMediaGlobal);
+        // Mostreo global//
+        System.out.println("\nEstadísticas generales:");
+        System.out.printf("Número de alumnos: %d\n", notAlum.size());
+        System.out.printf("Nota media general: %.2f\n", notaMediaGlobal);
 
-        System.out.println("\nNotas individuales de los alumnos");
-        for (String alumno : notAlum.keySet()) {
-            System.out.println(alumno + ": " + notAlum.get(alumno));
+        // Mostreo individual//
+        System.out.println("\nNotas individuales de los alumnos:");
+        for (String nombre : notAlum.keySet()) {
+            System.out.printf("%s: %.2f\n", nombre, notAlum.get(nombre));
         }
 
         sc.close();
