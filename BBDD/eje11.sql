948189ab7b87f37eeb60c3c74cf27a3c0faed21f
Kokushibou97 <136746018+Kokushibou97@users.noreply.github.com>
2024-04-16 11:01:43 +0200

    Update eje11.sql

--- BBDD/eje11.sql
+++ BBDD/eje11.sql
@@ -1,16 +1,16 @@
--- Crear la tabla FACULTAD
+
 CREATE TABLE FACULTAD (
     Codigo INT PRIMARY KEY,
     Nombre NVARCHAR(100)
 );
 
--- Crear la tabla INVESTIGADORES
+
 CREATE TABLE INVESTIGADORES (
     DNI VARCHAR(8) PRIMARY KEY,
     NomApels NVARCHAR(255)
 );
 
--- Crear la tabla RESERVA
+
 CREATE TABLE RESERVA (
     Facultad INT,
     Comienzo DATETIME,
@@ -19,7 +19,7 @@ CREATE TABLE RESERVA (
     FOREIGN KEY (Facultad) REFERENCES FACULTAD(Codigo)
 );
 
--- Crear la tabla EQUIPOS
+
 CREATE TABLE EQUIPOS (
     NumSerie CHAR(4) PRIMARY KEY,
     Nombre NVARCHAR(100),
