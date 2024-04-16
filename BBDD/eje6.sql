7959ae40bce66aa037b6a00b36917b5400b3f717
Kokushibou97 <136746018+Kokushibou97@users.noreply.github.com>
2024-04-16 11:02:00 +0200

    Update eje6.sql

--- BBDD/eje6.sql
+++ BBDD/eje6.sql
@@ -1,10 +1,10 @@
--- Crear la tabla PELICULAS
+
 CREATE TABLE PELICULAS (
     Codigo INT AUTO_INCREMENT PRIMARY KEY,
     Nombre NVARCHAR(100),
     CalificacionEdad INT
 );
--- Crear la tabla SALAS
+
 CREATE TABLE SALAS (
     Codigo INT AUTO_INCREMENT PRIMARY KEY,
     Nombre NVARCHAR(100),
