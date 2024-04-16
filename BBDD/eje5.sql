88fe884d5bbd0e3a1dc012bb19bba95eb788be0a
Kokushibou97 <136746018+Kokushibou97@users.noreply.github.com>
2024-04-16 10:37:33 +0200

    update

--- BBDD/eje5.sql
+++ BBDD/eje5.sql
@@ -1,11 +1,8 @@
--- Crear la tabla ALMACENES
 CREATE TABLE ALMACENES (
     Codigo INT AUTO_INCREMENT PRIMARY KEY,
     Lugar NVARCHAR(100),
     Capacidad INT
 );
-
--- Crear la tabla CAJAS
 CREATE TABLE CAJAS (
     NumReferencia CHAR(5) PRIMARY KEY,
     Contenido NVARCHAR(100),
