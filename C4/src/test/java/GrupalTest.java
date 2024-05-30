b1b0c29918344d364495bf526507de89c17c7abb
Kokushibou97 <136746018+Kokushibou97@users.noreply.github.com>
2024-05-30 10:11:46 +0200

    update

---
+++ C4/src/test/java/GrupalTest.java
@@ -0,0 +1,309 @@
+import junit.framework.TestCase;
+
+public class GrupalTest extends TestCase {
+
+	public final void testGrupal() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetGraphics() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testUpdate() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testRepaintLongIntIntIntInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testParamString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetAccessibleContext() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testAddImpl() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testRemoveComponent() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetLayout() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetIconImage() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testProcessWindowEvent() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testJFrame() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testJFrameGraphicsConfiguration() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testJFrameString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testJFrameStringGraphicsConfiguration() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testFrameInit() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testCreateRootPane() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetDefaultCloseOperation() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetDefaultCloseOperation() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetTransferHandler() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetTransferHandler() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetJMenuBar() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetJMenuBar() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testIsRootPaneCheckingEnabled() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetRootPaneCheckingEnabled() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetRootPane() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetRootPane() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetContentPane() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetContentPane() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetLayeredPane() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetLayeredPane() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetGlassPane() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetGlassPane() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetDefaultLookAndFeelDecorated() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testIsDefaultLookAndFeelDecorated() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testConstructComponentName() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetBackground() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testAddNotify() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testRemoveNotify() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testRemoveMenuComponent() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testPostProcessKeyEvent() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetOpacity() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetShape() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testFrame() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testFrameGraphicsConfiguration() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testFrameString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testFrameStringGraphicsConfiguration() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetTitle() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetTitle() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetIconImage() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetMenuBar() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetMenuBar() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testIsResizable() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetResizable() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetState() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetExtendedState() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetState() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetExtendedState() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetMaximizedBounds() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetMaximizedBounds() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetUndecorated() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testIsUndecorated() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSetCursorInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetCursorType() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetFrames() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetClass() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testHashCode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testEquals() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testClone() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testNotify() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testNotifyAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testWait() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testWaitLong() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testWaitLongInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testFinalize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
