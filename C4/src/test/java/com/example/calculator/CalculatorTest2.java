6f4207f62cb1c798d93bff237325c8d12c494b1c
Kokushibou97 <136746018+Kokushibou97@users.noreply.github.com>
2024-05-30 12:11:55 +0200

    update

---
+++ C4/src/test/java/com/example/calculator/CalculatorTest2.java
@@ -0,0 +1,75 @@
+package com.example.calculator;
+
+import junit.framework.TestCase;
+
+public class CalculatorTest2 extends TestCase {
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	public final void testAdd() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testSubtract() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testMultiply() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testGetClass() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testHashCode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testEquals() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testClone() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testNotify() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testNotifyAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testWait() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testWaitLong() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testWaitLongInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	public final void testFinalize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
